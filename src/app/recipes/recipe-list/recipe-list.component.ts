f933472e06704b66b4a464c22477dc864d184585
OmarMoussamih <omarmoussamih5@gmail.com>
2024-02-27 16:49:35 +0100

    set-up basic component content

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://1.bp.blogspot.com/-p21fd82K4X4/WtVw0Wl2_JI/AAAAAAAAE6Q/m8u8gXxbhvUEYemlNctIP0sSUaB7AaW-QCLcBGAs/s1600/IMG_6020.JPG'), 
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://1.bp.blogspot.com/-p21fd82K4X4/WtVw0Wl2_JI/AAAAAAAAE6Q/m8u8gXxbhvUEYemlNctIP0sSUaB7AaW-QCLcBGAs/s1600/IMG_6020.JPG')
+  ];
 
 }
--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+
+  ingredients:Ingredient[] = [
+    new Ingredient('Tomato', 5),
+    new Ingredient('Potato', 4)
+  ];
 
 }
